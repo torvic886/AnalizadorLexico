25fdb4b41a3c0421e892bb7b0d55acb3c7903963
Victor <vagutierrezv@uqvirtual.edu.co>
2020-10-12 22:18:48 -0500

    Se realiza la interfaz grafica del analizador lexico con sus respectivas validaciones

--- src/co/edu/uniquindio/compiladores/controladores/InicioController.kt
+++ src/co/edu/uniquindio/compiladores/controladores/InicioController.kt
@@ -1,20 +1,76 @@
 package co.edu.uniquindio.compiladores.controladores
 
+import co.edu.uniquindio.compiladores.lexico.AnalizadorLexico
+import co.edu.uniquindio.compiladores.lexico.Token
+import javafx.collections.FXCollections
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
 import javafx.scene.control.TextArea
 import javafx.scene.control.TextField
+import javafx.scene.control.cell.PropertyValueFactory
+import java.net.URL
+import java.util.*
+import javax.swing.JOptionPane
 
-class InicioController
+class InicioController : Initializable
 {
-    @FXML lateinit var areaTexto:TextArea
-    @FXML lateinit var txtPrueba:TextField
+    @FXML lateinit var codigoFuente:TextArea
 
-    fun cambiarValores( e:ActionEvent)
+    @FXML lateinit var tablaTokens: TableView<Token>
+    @FXML lateinit var lexemaToken: TableColumn<Token, String>
+    @FXML lateinit var categoriaToken: TableColumn<Token, String>
+    @FXML lateinit var filaToken: TableColumn<Token, Int>
+    @FXML lateinit var columnaToken: TableColumn<Token, Int>
+
+    @FXML
+    fun analizar( e:ActionEvent)
     {
-        val aux = areaTexto.text
-        areaTexto.text = txtPrueba.text
-        txtPrueba.text = aux
+        if (codigoFuente.text.isNotEmpty())
+        {
+            val lexico = AnalizadorLexico(codigoFuente.text)
+            lexico.analizar()
+            tablaTokens.items = FXCollections.observableArrayList(lexico.listaTokens)
+            print(lexico.listaTokens)
+        }
+        else
+        {
+          //  JOptionPane.showMessageDialog(null, "Al menos Debe Ingresar Un Codigo");
+            JOptionPane.showMessageDialog(null, "Al menos Debe Ingresar Un Codigo",
+                    "Mensaje Informativo", JOptionPane.WARNING_MESSAGE);
+        }
+
     }
-    
+
+    @FXML
+    fun limpiar( e:ActionEvent)
+    {
+
+        if (codigoFuente.text.isNotEmpty())
+        {
+
+            tablaTokens.items.clear()
+            codigoFuente.text = ""
+        }
+        else
+        {
+            //  JOptionPane.showMessageDialog(null, "Al menos Debe Ingresar Un Codigo");
+            JOptionPane.showMessageDialog(null, "No existe Un Codigo Que Limpiar",
+                    "Mensaje Informativo", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+    }
+
+
+    override fun initialize(location: URL?, resources: ResourceBundle?)
+    {
+        lexemaToken.cellValueFactory = PropertyValueFactory("lexema")
+        categoriaToken.cellValueFactory = PropertyValueFactory("categoria")
+        filaToken.cellValueFactory = PropertyValueFactory("fila")
+        columnaToken.cellValueFactory = PropertyValueFactory("columna")
+
+    }
+
 }
