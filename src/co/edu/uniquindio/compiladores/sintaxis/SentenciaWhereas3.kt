b4f00ab5125d5d4c25ae5bd0ffcbe1fe2939471f
mauricio1105 <mtabaresm@uqvirtual.edu.co>
2020-12-02 21:58:48 -0500

    se agregaron nuevos metodos

--- src/co/edu/uniquindio/compiladores/sintaxis/SentenciaWhereas3.kt
+++ src/co/edu/uniquindio/compiladores/sintaxis/SentenciaWhereas3.kt
@@ -1,28 +1,40 @@
 package co.edu.uniquindio.compiladores.sintaxis
 
 import co.edu.uniquindio.compiladores.lexico.Error
+import co.edu.uniquindio.compiladores.semantica.Ambito
 import co.edu.uniquindio.compiladores.semantica.TablaSimbolos
 import javafx.scene.control.TreeItem
 
-class SentenciaWhereas3(var condicion: Condicion3, var sentencias: ArrayList<Sentencia3>):Sentencia3(){
+class SentenciaWhereas3(var condicion: Condicion3, var sentencias: ArrayList<Sentencia3>) : Sentencia3() {
     override fun toString(): String {
         return "SentenciaWhereas2(condicion=$condicion, sentencias=$sentencias)"
     }
+
     override fun getArbolVisual(): TreeItem<String> {
         var raiz = TreeItem("Sentencia Whereas")
 
-        raiz.children.add( condicion.getArbolVisual() )
+        raiz.children.add(condicion.getArbolVisual())
 
         if (sentencias.size >= 1) {
             for (s in sentencias) {
-                raiz.children.add( s.getArbolVisual() )
+                raiz.children.add(s.getArbolVisual())
             }
         }
         return raiz
     }
-    override fun llenarTablaSimbolos(tablaSimbolos: TablaSimbolos, listaErrores: ArrayList<Error>, ambito: String ) {
-        for ( s in sentencias ) {
+
+    override fun llenarTablaSimbolos(tablaSimbolos: TablaSimbolos, listaErrores: ArrayList<Error>, ambito: Ambito) {
+        for (s in sentencias) {
             s.llenarTablaSimbolos(tablaSimbolos, listaErrores, ambito)
         }
     }
+
+    override fun analizarSemantica(tablaSimbolos: TablaSimbolos, listaErrores: ArrayList<Error>, ambito: Ambito) {
+        condicion.analizarSemantica(tablaSimbolos, listaErrores, ambito)
+        for (s in sentencias) {
+            s.analizarSemantica(tablaSimbolos, listaErrores, ambito)
+        }
+    }
+
+
 }
